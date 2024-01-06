0b19015e7b22a1ef22c54c00dec2099acbff6dcb
parkjungsub <eses565@naver.com>
2024-01-07 05:11:21 +0900

    3 장바구니 완성

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         ProductList list = new ProductList();
         Menu menuList = new Menu();
         Scanner sc = new Scanner(System.in);
-        ArrayList<Menu> orderList = new ArrayList<Menu>(); //장바구니
+        ArrayList<Menu> orderList = new ArrayList<Menu>(); //장바구니 객체 배열
         int select;
         int count = 0;
         double sum = 0;
@@ -25,75 +25,72 @@ public class Main {
       //  System.out.println(orderList.get(0).getExplanation());
 
 
-        System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
+        System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));// 메뉴판 출력
 
         while (true){
             int option = sc.nextInt();
             switch (option){
                 case 1:
-                    list.getBurgerList();
+                    list.getBurgerList();// 버거의 선택 리스트 출력
                     select = sc.nextInt();
                     list.getBurger(select);
                     System.out.println(menuTest.getScreenMenu(MenuIndex.CART));
-                    select = sc.nextInt();
+                    option =sc.nextInt();
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
-                    orderList.add(list.getBurgerList(option));
-                    if(select == 1){
-                        sum = orderList.get(count).getPrice();
+                    orderList.add(list.getBurgerList(select -1));
+                    if(option == 1){
+                        //sum = orderList.get(count).getPrice();
                         System.out.println("장바구니에 추가되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
-                    }else if(select == 2){
+                    }else if(option == 2){
                         System.out.println("장바구니 취소되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
                 case 2:
-                    list.getShakeList();
+                    list.getShakeList();// 쉐이크 선택 리스트 출력
                     select = sc.nextInt();
                     list.getShake(select);
                     System.out.println(menuTest.getScreenMenu(MenuIndex.CART));
-                    select = sc.nextInt();
+                    option = sc.nextInt();
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
-                    orderList.add(list.getshakeList(select));
-                    if(select == 1){
-                        orderList.add(list.getshakeList(option));
+                    orderList.add(list.getshakeList(select - 1));
+                    if(option == 1){
                         System.out.println("장바구니에 추가되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
-                    }else if(select == 2){
+                    }else if(option == 2){
                         System.out.println("장바구니 취소되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
                 case 3:
-                    list.getDrinkList();
+                    list.getDrinkList();// 드링크 선택 리스트 출력
                     select = sc.nextInt();
                     list.getDrink(select);
                     System.out.println(menuTest.getScreenMenu(MenuIndex.CART));
-                    select = sc.nextInt();
+                    option = sc.nextInt();
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
-                    orderList.add(list.getDrinkList(select));
-                    if(select == 1){
-                        orderList.add(list.getDrinkList(option));
+                    orderList.add(list.getDrinkList(select -1));
+                    if(option == 1){
                         System.out.println("장바구니에 추가되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
-                    }else if(select == 2){
+                    }else if(option == 2){
                         System.out.println("장바구니 취소되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
                 case 4:
-                    list.getBeerList();
+                    list.getBeerList(); // 맥주 선택 리스트 출력
                     select = sc.nextInt();
                     list.getBeer(select);
                     System.out.println(menuTest.getScreenMenu(MenuIndex.CART));
-                    select = sc.nextInt();
+                    option = sc.nextInt();
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
-                    orderList.add(list.getBeerList(select));
-                    if(select == 1){
-                        orderList.add(list.getBeerList(option));
+                    orderList.add(list.getBeerList(select -1));
+                    if(option == 1){
                         System.out.println("장바구니에 추가되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
-                    }else if(select == 2){
+                    }else if(option == 2){
                         System.out.println("장바구니 취소되었습니다.");
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
