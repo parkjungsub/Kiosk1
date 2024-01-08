40c32152e48c9814c1c87ae4d76ca24dd2508f98
parkjungsub <eses565@naver.com>
2024-01-08 12:22:01 +0900

    1차 수정

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,28 @@
 import menu.Menu;
 import menu.MenuIndex;
 import menu.ScreenMenu;
-import order.Order;
 import product.*;
-import order.Order;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ScreenMenu menuTest = new ScreenMenu();
-        ProductList list = new ProductList();
-        Menu menuList = new Menu();
+        ScreenMenu menuTest = new ScreenMenu(); //메뉴판 출력 객체
+        ProductList list = new ProductList(); // 상품(burger, drink, beer, shake)의 종류 객체
         Scanner sc = new Scanner(System.in);
         ArrayList<Menu> orderList = new ArrayList<Menu>(); //장바구니 객체 배열
-        int select;
-        int count = 0;
-        double sum = 0;
+        int select; //
+        int count = 0; // 장바구니 들어간 횟수
+        double sum = 0; // 장바구니의 값을 더하는 변수
+        int order = 1; // 주문횟수
 
-       // orderList.add(list.getBurgerList(0));
-
-      //  System.out.println(orderList.get(0).getExplanation());
-
-
-        System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));// 메뉴판 출력
+        System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));// 첫 메뉴판 출력\
 
         while (true){
             int option = sc.nextInt();
             switch (option){
-                case 1:
+                case 1: // 버거 선택하기
                     list.getBurgerList();// 버거의 선택 리스트 출력
                     select = sc.nextInt();
                     list.getBurger(select); //select 입력 받은 숫자의 리스트 출력
@@ -47,7 +39,7 @@ public class Main {
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
-                case 2:
+                case 2: //쉐이크 선택하기
                     list.getShakeList();// 쉐이크 선택 리스트 출력
                     select = sc.nextInt();
                     list.getShake(select); //select 입력 받은 숫자의 리스트 출력
@@ -64,7 +56,7 @@ public class Main {
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
-                case 3:
+                case 3:// 음표 선택하기
                     list.getDrinkList();// 드링크 선택 리스트 출력
                     select = sc.nextInt();
                     list.getDrink(select); //select 입력 받은 숫자의 리스트 출력
@@ -81,14 +73,16 @@ public class Main {
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                 break;
-                case 4:
+                case 4: //맥주 선택하기
                     list.getBeerList(); // 맥주 선택 리스트 출력
                     select = sc.nextInt();
                     list.getBeer(select); //select 입력 받은 숫자의 리스트 출력
                     System.out.println(menuTest.getScreenMenu(MenuIndex.CART));
                     option = sc.nextInt();// 장바구니에 담을 것인지 아닌지 입력 받기
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
-                    if(option == 1){
+
+
+                    if(option == 1){  //
                         orderList.add(list.getBeerList(select -1));// 장바구니에 선택한 리스트 넣기
                         sum += orderList.get(count).getPrice(); //장바구니에 담긴 금액 합계 더하기
                         System.out.println("장바구니에 추가되었습니다.");
@@ -98,14 +92,25 @@ public class Main {
                         System.out.println(menuTest.getScreenMenu(MenuIndex.MAIN));
                     }
                     break;
-                case 5:
+
+
+                case 5: // 장바구니에 있는 목록 주문하기
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER));
                     for(int i = 0; i < orderList.size() ;i++){ // 장바구니 리스트 출력하기
                         System.out.println(orderList.get(i).getName()+ "     | W" +orderList.get(i).getPrice() +" | " + orderList.get(i).getExplanation());
                     }
-                    System.out.println("[Total]"+"\nW" + sum); // 장바구니 합계 출력
+                    System.out.println("\n[Total]"+"\n W" + sum +"\n"); // 장바구니 합계 출력
+                    System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER_OPTION));
+                    option = sc.nextInt();
+                    if(option == 1){
+                        System.out.println("주문이 완료되었습니다! \n 대기번호는 [ " + order + " ] 번 입니다" );
+                        order++;
+                    }else {
+                        System.out.println("주문이 취소 되었습니다.");
+                }
                     break;
-                case 6:
+
+                case 6: //주문취소하기
                     System.out.println(menuTest.getScreenMenu(MenuIndex.ORDER_CANCEL));
                     if (option == 1){
                         orderList.clear();
--- src/product/Beer.java
+++ src/product/Beer.java
@@ -7,7 +7,7 @@ public class Beer extends Menu {
     public Beer(String name, String explanation, double price){ //super 빠꾸자
         super(name, explanation, price);
     }
-//
+
 //    public String getName(){
 //        return name;
 //    }
